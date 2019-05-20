136142740bc612be2e5b676b85e20c659ed5232a
Letícia Fernandes <leticiafs1994@hotmail.com>
2019-05-20 16:44:48 -0300

    Adding devise; changingstyle

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
     render :layout => false
+    end
+  def home
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  authenticated :user do
+    root 'home#home', as: :authenticated_root
+  end
+
   root 'home#index', as: 'home_index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
