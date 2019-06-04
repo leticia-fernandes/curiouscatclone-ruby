15e583692b6d9e90dec37fa88a5e09740147df58
Letícia Fernandes <leticiafs1994@hotmail.com>
2019-06-04 15:55:34 -0300

    Add Like feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,18 +8,21 @@ class ApplicationController < ActionController::Base
   end
 
   private
+
   # Overwriting the sign_out redirect path method
   def after_sign_out_path_for(_resource_or_scope)
     new_user_session_path
   end
 
   private
+
   # Overwriting the sign_in redirect path method
   def after_sign_in_path_for(resource)
     stored_location_for(resource) || me_path
   end
 
   protected
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[username name])
   end
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,5 +2,11 @@ class Answer < ApplicationRecord
   belongs_to :question
   has_many :likes
 
+  scope :liked, -> { joins(:likes) }
+
   validates :content, :question_id, presence: true
+
+  def liked_by?(user)
+    likes.where(user: user).present?
+  end
 end
